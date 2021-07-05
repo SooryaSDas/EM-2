3b90ce8081217ffa6e49822cecc455f19dfc5441
SooryaSDas <83228601+SooryaSDas@users.noreply.github.com>
2021-07-05 13:37:29 +0530

    Add files via upload

--- conn/conn.js
+++ conn/conn.js
@@ -1,1 +1,16 @@
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'newdata'
+});
+
+con.connect((err) => {
+    if(err) throw err;
+    console.log('Database Connected..');
+});
+
+module.exports = con;
